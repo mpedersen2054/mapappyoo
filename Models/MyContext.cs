2a9b1c1247c0e3a5af73ea4551644b5d3f3f1bab
Max Rohrer <mxrohrer@gmail.com>
2017-02-27 17:43:35 -0800

    completed DB setup and context file

---
+++ Models/MyContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+namespace mappApp.Models
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        { }
+ 
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Groups {get; set;}
+        public DbSet<Location> Locations {get; set;}
+        public DbSet<Category> Categories {get; set;}
+        public DbSet<Review> Reviewes {get; set;}
+        public DbSet<UserGroup> UserGroups {get; set;}
+        public DbSet<LocationCategory> LocationCategories {get; set;}
+        public DbSet<GroupLocation> GroupLocations {get; set;}
+
+
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,10 @@
-using System;
+using mapapp.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MySQL.Data.EntityFrameworkCore.Extensions;
 
 namespace mapapp
 {
@@ -13,6 +16,7 @@ namespace mapapp
             // Add framework services.
             services.AddMvc();
             services.AddSession();
+            services.AddDbContext<MyContext>(options => options.UseMySQL(Configuration["DBInfo:ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,6 +27,18 @@ namespace mapapp
             app.UseStaticFiles();
             app.UseSession();
             app.UseMvc();
+        }
+
+        public IConfiguration Configuration { get; private set; }
+ 
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+            Configuration = builder.Build();
+            
         }
     }
 }
