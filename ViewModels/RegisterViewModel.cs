1d02a159a1de7ec238f74eda0e204e01e800ddea
Max Rohrer <mxrohrer@gmail.com>
2017-03-01 20:03:43 -0800

    fixed some form stuff

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -16,7 +16,7 @@ namespace mapapp.ViewModels{
         [RegularExpressionAttribute(@"(?=^.{8,30}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;&quot;:;'?/&gt;.&lt;,]).*$", ErrorMessage = "Password must be 8 charactes long and contain at least 1 of each: uppercase letter, lowercase letter, number, and special character.")]
         public string Password {get; set;}
         [DataTypeAttribute(DataType.Password)]
-        [CompareAttribute("password", ErrorMessage = "Passwords must match")]
+        [CompareAttribute("Password", ErrorMessage = "Passwords must match")]
         public string PasswordConfirmation {get; set;}
         [RequiredAttribute(ErrorMessage="Please include a link to a profile picture.")]
         public string ProfilePic {get; set;}
