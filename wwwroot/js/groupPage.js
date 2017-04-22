f8fe3391571b2fa74bc76ed934780e65c1a459ab
Matt Pedersen <matthewnpedersen@gmail.com>
2017-04-22 14:54:55 -0700

    dunno

--- wwwroot/js/groupPage.js
+++ wwwroot/js/groupPage.js
@@ -7,15 +7,8 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// L.marker([47.606438, -122.132453]).addTo(map)
-//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//     .openPopup();
-
-
 function reqLocations(groupId, callback) {
     $.get('/getGroupLocations/' + groupId, function(data) {
-        // console.log('INSIDE POST REQ!!!')
-        // console.log(data)
         callback(null, data)
     })
 }
@@ -23,8 +16,6 @@ function reqLocations(groupId, callback) {
 reqLocations($('.group-id').data('groupid'), function(err, data) {
     var pData = JSON.parse(data)
     var groupLocs = pData.GroupLocs
-
-    console.log(groupLocs)
 
     for (var i = 0; i < groupLocs.length; i++) {
         var curr = groupLocs[i].GroupLoc
@@ -40,28 +31,7 @@ reqLocations($('.group-id').data('groupid'), function(err, data) {
         L.marker([curr.Lat, curr.Lng]).addTo(map)
             .bindPopup(tmpl)
             .openPopup();
-        // })
     }
-    // console.log(pData)
 })
 
 })
-// var moves = 0
-// var ticks = 0
-
-// map.on('move', function(e) {
-//     moves++
-//     if (moves % 40 == 0) {
-//         ticks++
-//         var nm = map.getBounds()
-//         var ne = nm._northEast
-//         var sw = nm._southWest
-//         console.log(ne.lat, ne.lng)
-//         console.log(sw.lat, sw.lng)
-//         console.log(ticks)
-//     }
-// })
-
-// map.on('click', function(ev) {
-//     console.log(map); // ev is an event object (MouseEvent in this case)
-// })
